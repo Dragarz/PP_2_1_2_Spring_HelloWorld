42eb279fb99d7fb993dbc5c23e4c107a42204231
levin <ivan.levin@ocrv.ru>
2025-05-22 15:33:03 +0300

    add POJO class Cat and compare cat beans

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,17 @@
-public class Cat {
+import java.io.Serializable;
+
+public class Cat implements Serializable {
+    private String say;
+
+    public Cat() {
+        say = "мяу";
+    }
+
+    public String getSay() {
+        return say;
+    }
+
+    public void setSay(String say) {
+        this.say = say;
+    }
 }
